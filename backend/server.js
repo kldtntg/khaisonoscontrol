ef6f193dd835cf7b545a40d7e57728c12e1ac584
Khai <kldtntg@gmail.com>
2025-08-25 20:18:33 -0400

    bug fixes

--- backend/server.js
+++ backend/server.js
@@ -49,4 +49,6 @@ app.post("/exchange-token", async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log("Backend running on port 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
+
