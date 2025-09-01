1cb5e7a78caa9ca4a0a5654b500c1abca6a8bc15
Khai <kldtntg@gmail.com>
2025-09-01 15:41:36 -0400

    change type to module

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
-const express = require("express");
-const fetch = require("node-fetch");
-const bodyParser = require("body-parser");
-require("dotenv").config();
-const path = require("path");
+import express from "express";
+import fetch from "node-fetch";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 
+dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(bodyParser.json());
@@ -14,7 +19,7 @@ app.use(express.static(path.join(__dirname, "../frontend")));
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 
-console.log('khai', process.env.CLIENT_ID, process.env.CLIENT_SECRET)
+console.log('khai', CLIENT_ID, CLIENT_SECRET);
 
 // Endpoint to generate Sonos auth URL
 app.post("/auth-url", (req, res) => {
@@ -24,9 +29,10 @@ app.post("/auth-url", (req, res) => {
   res.json({ url });
 });
 
+// Endpoint to exchange auth code for access token
 app.post("/exchange-token", async (req, res) => {
   const { code, redirect_uri } = req.body;
-  console.log('khai', code, redirect_uri)
+  console.log('khai', code, redirect_uri);
 
   try {
     const params = new URLSearchParams();
@@ -37,9 +43,7 @@ app.post("/exchange-token", async (req, res) => {
     const response = await fetch("https://api.sonos.com/login/v3/oauth/access", {
       method: "POST",
       headers: {
-        "Authorization": "Basic " + Buffer.from(
-          process.env.CLIENT_ID + ":" + process.env.CLIENT_SECRET
-        ).toString("base64"),
+        "Authorization": "Basic " + Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString("base64"),
         "Content-Type": "application/x-www-form-urlencoded"
       },
       body: params
@@ -60,7 +64,5 @@ app.post("/exchange-token", async (req, res) => {
   }
 });
 
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
-
